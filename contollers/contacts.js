df39ee83474dd0a091974bf83ffaa2a1f5a268f1
Maksym-Kovorotnyi <Kovorotny1@gmail.com>
2023-04-20 16:39:02 +0300

    final

--- contollers/contacts.js
+++ contollers/contacts.js
@@ -1,15 +1,9 @@
-const {
-  listContacts,
-  getContactById,
-  addContact,
-  removeContact,
-  updateContact,
-} = require("../models/contacts");
+const { Contact } = require("../models/contact");
 const HttpError = require("../helpers/HttpError");
 const ctrlWrapper = require("../helpers/ctrlWrapper");
 
 const getAllContacts = async (req, res) => {
-  const result = await listContacts();
+  const result = await Contact.find();
   if (!result) {
     throw HttpError(404, "Not found");
   }
@@ -18,7 +12,7 @@ const getAllContacts = async (req, res) => {
 
 const getOneContact = async (req, res) => {
   const { contactId } = req.params;
-  const result = await getContactById(contactId);
+  const result = await Contact.findById(contactId);
   if (!result) {
     throw HttpError(404, "There is no such contact");
   }
@@ -27,13 +21,13 @@ const getOneContact = async (req, res) => {
 
 const addNewContact = async (req, res) => {
   const { name, email, phone } = req.body;
-  const result = await addContact(name, email, phone);
+  const result = await Contact.create({ name, email, phone });
   res.status(201).json(result);
 };
 
 const deleteContact = async (req, res) => {
   const { contactId } = req.params;
-  const result = await removeContact(contactId);
+  const result = await Contact.findByIdAndRemove(contactId);
   if (!result) {
     throw HttpError(404, "There is no such Id");
   }
@@ -42,7 +36,18 @@ const deleteContact = async (req, res) => {
 
 const changeContact = async (req, res) => {
   const id = req.params.contactId;
-  const result = await updateContact(id, req.body);
+  const result = await Contact.findByIdAndUpdate(id, req.body, { new: true });
+  res.json(result);
+};
+
+const updateFavorites = async (req, res, next) => {
+  const id = req.params.contactId;
+  const result = await Contact.findByIdAndUpdate(id, req.body, {
+    new: true,
+  });
+  if (!result) {
+    throw HttpError(404, "There is no such Id");
+  }
   res.json(result);
 };
 
@@ -52,4 +57,5 @@ module.exports = {
   addNewContact: ctrlWrapper(addNewContact),
   deleteContact: ctrlWrapper(deleteContact),
   changeContact: ctrlWrapper(changeContact),
+  updateFavorites: ctrlWrapper(updateFavorites),
 };
--- middlewares/index.js
+++ middlewares/index.js
@@ -1,5 +1,7 @@
 const { contactsValidateBody } = require("./contactsValidateBody");
+const { isvalidId } = require("./isValidId");
 
 module.exports = {
   contactsValidateBody,
+  isvalidId,
 };
